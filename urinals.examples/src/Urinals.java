247b1149c627d8891ebe681d0ed46c454df9f4e1
manideepnalluri12 <mnallur2@asu.edu>
2022-10-28 08:50:32 -0700

    Implementing Skeleton code

---
+++ urinals.examples/src/Urinals.java
@@ -0,0 +1,10 @@
+//@Author-Manideep Nalluri
+
+    public class Urinals{
+        static Boolean urinals(){
+            System.out.println("Not Implemented");
+            return true;
+        }
+        public static void main(String[] args){urinals();}
+    }
+
